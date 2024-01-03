a1566d905e705eecc361bcfc53c6169cef795d3b
YouDevs <carlos_develops@outlook.com>
2024-01-03 17:58:23 -0600

    installed Guzzle package and testing tracking created

--- app/Http/Controllers/CustomerSupport/OrderController.php
+++ app/Http/Controllers/CustomerSupport/OrderController.php
@@ -12,6 +12,8 @@ use App\Http\Requests\UpdateOrderInvoiceRequest;
 use App\Models\Order;
 use Illuminate\Support\Facades\Storage;
 use App\Enums\DeliveryServicesEnum;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
@@ -50,14 +52,37 @@ class OrderController extends Controller
 
     public function updateGuideNumber(UpdateOrderGuideNumberRequest $request, Order $order)
     {
-        $order->delivery_service = $request->delivery_service;
-        $order->guide_number = $request->guide_number;
-        $order->save();
 
-        session()->flash('message', 'Número de guía de la orden actualizado exitosamente');
-        session()->flash('icon', 'success');
+        Log::info( $request->all() );
 
-        return redirect()->route('admin.orders.index');
+        $client = new Client();
+
+        $api_key = env("TRACKING_MORE_API_KEY");
+        Log::info("api_key: $api_key");
+
+        $response = $client->request('POST', 'https://api.trackingmore.com/v4/trackings/create', [
+            'headers' => [
+                'Tracking-Api-Key' => $api_key,
+                'Content-Type' => 'application/json'
+            ],
+            'json' => [
+                'tracking_number' =>  $request->guide_number, //$trackingNumber,
+                'courier_code' =>  $request->delivery_service //$courierCode
+            ]
+        ]);
+
+        Log::info($response->getBody());
+
+        return json_decode($response->getBody(), true);
+
+        // $order->delivery_service = $request->delivery_service;
+        // $order->guide_number = $request->guide_number;
+        // $order->save();
+
+        // session()->flash('message', 'Número de guía de la orden actualizado exitosamente');
+        // session()->flash('icon', 'success');
+
+        // return redirect()->route('admin.orders.index');
     }
 
     public function updateInvoice(UpdateOrderInvoiceRequest $request, Order $order)
